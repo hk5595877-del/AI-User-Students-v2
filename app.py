90eb206946a2e426b92790a8aab8c93e69df78ea
Haris Ali Khan <hk5595877@gmail.com>
2026-08-27 22:21:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -826,18 +826,18 @@ uploaded_file = st.file_uploader(
 # RE-PREDICT BUTTON
 # ==========================================
 
-repredict = st.button(
-    "🔄 Re-predict",
-    use_container_width=True
-)
-st.caption(
-    "⚠️ Your CSV must contain these exact columns:"
+    repredict = st.button(
+        "🔄 Re-predict",
+        use_container_width=True
     )
+    st.caption(
+        "⚠️ Your CSV must contain these exact columns:"
+        )
 
-"(Major_Category, Year_of_Study, Pre_Semester_GPA, Weekly_GenAI_Hours, Primary_Use_Case, Prompt_Engineering_Skill,"
-"Tool_Diversity, Paid_Subscription, Traditional_Study_Hours, Perceived_AI_Dependency, Institutional_Policy,"
-"Anxiety_Level_During_Exams)",
-language="text"
+    "(Major_Category, Year_of_Study, Pre_Semester_GPA, Weekly_GenAI_Hours, Primary_Use_Case, Prompt_Engineering_Skill,"
+    "Tool_Diversity, Paid_Subscription, Traditional_Study_Hours, Perceived_AI_Dependency, Institutional_Policy,"
+    "Anxiety_Level_During_Exams)",
+    language="text"
 
     if uploaded_file is None:
         st.write(
